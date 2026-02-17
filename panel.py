486390bcd71e277badd8a6f8e40a13d037ea9532
hsnklc044-jpg <hsnklc044@gmail.com>
2026-02-18 00:50:48 +0300

    Update panel.py

--- panel.py
+++ panel.py
@@ -4,23 +4,28 @@ import requests
 
 app = Flask(__name__)
 
-# DOĞRU kullanım
 TELEGRAM_TOKEN = os.environ.get("8500392750:AAHprLseYkYGlF6zTw8YJ4doFLqwwvOSjVM")
 
 
 @app.route("/")
 def home():
-    return "OK"
+    return "OK", 200
 
 
 def send_telegram(chat_id, message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-    requests.post(url, json={"chat_id": chat_id, "text": message}, timeout=10)
+    try:
+        requests.post(url, json={"chat_id": chat_id, "text": message}, timeout=10)
+    except Exception as e:
+        print("Telegram hata:", e)
 
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.get_json()
+    data = request.get_json(silent=True)
+
+    if not data:
+        return "no data", 200
 
     if "message" in data:
         chat_id = data["message"]["chat"]["id"]
