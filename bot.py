32877bf87f41154da5fa66ea6f4f9263febc96f6
hsnklc044-jpg <hsnklc044@gmail.com>
2026-02-18 10:49:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,18 +3,14 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🤖 BIST AI aktif.\nSistem çalışıyor.")
-
+    await update.message.reply_text("BIST AI aktif. Sistem çalışıyor.")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
-
 
 if __name__ == "__main__":
     main()
