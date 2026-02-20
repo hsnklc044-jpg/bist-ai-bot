a2ae9d05fd3bd4aa0d9e963db4f9a4c2629f4f7c
hsnklc044-jpg <hsnklc044@gmail.com>
2026-02-20 15:37:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = 8500392750:AAF6Z4VPanybEmsarVV8BZ_gz2coqnfxKZ8
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
