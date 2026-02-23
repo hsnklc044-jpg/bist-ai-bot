6c5140bbc397c1d575308fbc004a1e858970b4f2
hsnklc044-jpg <hsnklc044@gmail.com>
2026-02-23 16:13:19 +0300

    Update data_utils.py

--- app/data_utils.py
+++ app/data_utils.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 
 
 def get_data(symbol="SISE.IS"):
@@ -9,6 +10,10 @@ def get_data(symbol="SISE.IS"):
         return None
 
     df = df.reset_index()
+
+    if "Close" not in df.columns:
+        return None
+
     df.rename(columns={"Close": "close"}, inplace=True)
 
     return df
