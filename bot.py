663200129da3a377cfb89e876f2cc83d7ad68d00
hsnklc044-jpg <hsnklc044@gmail.com>
2026-02-24 17:29:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,26 @@
-from app.main import run
+import os
+import requests
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def run():
+
+    if not TOKEN or not CHAT_ID:
+        print("Environment değişkenleri eksik.")
+        return
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": "🚀 BOT TEMİZ KURULUM TEST MESAJI"
+    }
+
+    response = requests.post(url, json=payload)
+
+    print(response.text)
+
 
 if __name__ == "__main__":
     run()
