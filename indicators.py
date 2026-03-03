7c622af2a6ea175d500193eb66ba2a9893d7b5df
hsnklc044-jpg <hsnklc044@gmail.com>
2026-03-03 11:32:49 +0300

    Create indicators.py

---
+++ indicators.py
@@ -0,0 +1,24 @@
+# indicators.py
+
+import pandas as pd
+
+def add_indicators(df):
+
+    df["EMA20"] = df["Close"].ewm(span=20).mean()
+    df["EMA50"] = df["Close"].ewm(span=50).mean()
+    df["EMA200"] = df["Close"].ewm(span=200).mean()
+
+    delta = df["Close"].diff()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.rolling(14).mean()
+    avg_loss = loss.rolling(14).mean()
+
+    rs = avg_gain / avg_loss
+    df["RSI"] = 100 - (100 / (1 + rs))
+
+    df["Volume_MA20"] = df["Volume"].rolling(20).mean()
+    df["RelVolume"] = df["Volume"] / df["Volume_MA20"]
+
+    return df
