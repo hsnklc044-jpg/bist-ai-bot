99a3d904af6b69d0717c8b4a42e7c5683e296085
hsnklc044-jpg <hsnklc044@gmail.com>
2026-03-07 18:12:19 +0300

    Create telegram_engine.py

---
+++ telegram_engine.py
@@ -0,0 +1,16 @@
+import requests
+
+TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
+
+def send_telegram(message):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    requests.post(url, data=payload)
