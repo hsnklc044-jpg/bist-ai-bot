1be217a962a71acde4f129000e254beb2227e22f
hsnklc044-jpg <hsnklc044@gmail.com>
2026-03-07 18:08:33 +0300

    Create market_registry.py

---
+++ market_registry.py
@@ -0,0 +1,19 @@
+MARKETS = {
+
+    "BIST": [
+        "TUPRS.IS",
+        "THYAO.IS",
+        "EREGL.IS"
+    ],
+
+    "US": [
+        "AAPL",
+        "MSFT",
+        "NVDA"
+    ],
+
+    "CRYPTO": [
+        "BTC-USD",
+        "ETH-USD"
+    ]
+}
