dccd70954a2949ca971de114317b94765cce2b8e
hsnklc044-jpg <hsnklc044@gmail.com>
2026-03-08 00:22:54 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,13 +1,14 @@
 import yfinance as yf
 import numpy as np
 
+# BIST hisse listesi
 BIST_SYMBOLS = [
-"THYAO.IS","ASELS.IS","EREGL.IS","KRDMD.IS","SISE.IS",
-"SASA.IS","TUPRS.IS","PGSUS.IS","BIMAS.IS","AKBNK.IS",
-"DOHOL.IS","ENJSA.IS","ENKAI.IS","GLYHO.IS","GUBRF.IS",
-"HALKB.IS","KORDS.IS","MGROS.IS","NTHOL.IS","ODAS.IS",
-"OYAKC.IS","SELEC.IS","SMRTG.IS","SOKM.IS","TATGD.IS",
-"TKFEN.IS","TRGYO.IS","TSKB.IS","ULKER.IS","VAKBN.IS","VESBE.IS"
+    "THYAO.IS","ASELS.IS","EREGL.IS","KRDMD.IS","SISE.IS",
+    "SASA.IS","TUPRS.IS","PGSUS.IS","BIMAS.IS","AKBNK.IS",
+    "DOHOL.IS","ENJSA.IS","ENKAI.IS","GLYHO.IS","GUBRF.IS",
+    "HALKB.IS","KORDS.IS","MGROS.IS","NTHOL.IS","ODAS.IS",
+    "OYAKC.IS","SELEC.IS","SMRTG.IS","SOKM.IS","TATGD.IS",
+    "TKFEN.IS","TRGYO.IS","TSKB.IS","ULKER.IS","VAKBN.IS","VESBE.IS"
 ]
 
 
@@ -19,13 +20,18 @@ def scan_market():
 
         try:
 
-            data = yf.download(ticker, period="3mo", interval="1d", progress=False)
+            data = yf.download(
+                ticker,
+                period="3mo",
+                interval="1d",
+                progress=False
+            )
 
             if data.empty:
                 continue
 
-            # KRİTİK SATIR
-            close_prices = data["Close"].values.ravel()
+            # 1 boyutlu veri düzeltmesi
+            close_prices = data["Close"].to_numpy().ravel()
 
             if len(close_prices) < 20:
                 continue
@@ -33,12 +39,13 @@ def scan_market():
             last_price = close_prices[-1]
             avg20 = np.mean(close_prices[-20:])
 
+            # Basit momentum filtresi
             if last_price > avg20:
 
                 signals.append({
                     "ticker": ticker,
-                    "price": round(float(last_price),2),
-                    "signal": "Momentum Break"
+                    "signal": "Momentum Break",
+                    "entry": round(float(last_price), 2)
                 })
 
         except Exception as e:
