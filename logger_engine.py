6f31a94c77c9c40ea2adc58c4d76e9c59b09a04d
hsnklc044-jpg <hsnklc044@gmail.com>
2026-03-07 23:07:24 +0300

    Update logger_engine.py

--- logger_engine.py
+++ logger_engine.py
@@ -1,7 +1,11 @@
 import logging
+import os
+
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 
 logging.basicConfig(
-    filename="bot.log",
+    filename="logs/bot.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
